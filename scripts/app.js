9ae00824123b0779364431962f55c7ae5bc332d3
abillig <avrambillig@gmail.com>
2016-07-29 15:37:51 -0400

    initial file structure set up

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+$(function(){
+    event.preventDefault();
+//   // When the user clicks the submit button,
+//     // Gather the user input from the textbox
+//     // Make the request and get back the json
+//     // Append the images onto the page
+$('input:submit').on("click", function getAjax(){
+      event.preventDefault();
+  debugger;
+  // var user_input = $('#artist_name').val()
+  //   // $.ajax({
+  //   //   method: "GET",
+  //   //   url: `https://api.spotify.com/v1/search?q=${user_input}&type=artist`
+  //   // }).done(function( data ) {
+  //   //   var artist = data.artists.items[0]
+  //   //   var artist_image = artist.images[0].url
+  //   //   $('.images').append(`<img src=${artist_image}>`)
+  //   //   });
+
+})
+// //
+})
