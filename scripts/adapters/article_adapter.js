7a4f94ff819ec720a9c1f0da6396f1bcbcf94caf
abillig <avrambillig@gmail.com>
2016-07-30 01:46:16 -0400

    added handlebars to render views and filtered search queries

--- scripts/adapters/article_adapter.js
+++ scripts/adapters/article_adapter.js
@@ -2,14 +2,20 @@ function article_adapter(search_term){
     event.preventDefault();
   return $.ajax({
     method: "GET",
-    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?&q=${search_term}?&api-key=6c40f5b5259d4eb28e241c5b3aed4a75`
+    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?&q="${search_term}"&fq=headline:${search_term}?&api-key=6c40f5b5259d4eb28e241c5b3aed4a75`
   }).done(function( data ) {
     var articles = data.response.docs
+    debugger;
     articles.forEach(function(article){
-      // if(article.multimedia[0]){
+       if(article.multimedia[0]){
+        new Article(article.headline.main, article.lead_paragraph,
+        article.pub_date, article.web_url, search_term, article.multimedia[0].url)
+       }
+       else {
         new Article(article.headline.main, article.lead_paragraph,
         article.pub_date, article.web_url, search_term)
-      // }
+       }
     })
+    render()
   })
 }
