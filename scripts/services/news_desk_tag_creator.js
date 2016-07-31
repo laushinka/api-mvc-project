54e07781015cc46f85c79844d0e8e817c64dec4b
ToddRizley <todd.rizley@gmail.com>
2016-07-31 13:47:37 -0400

    update type model, article model, addToType function

--- scripts/services/news_desk_tag_creator.js
+++ scripts/services/news_desk_tag_creator.js
@@ -1,8 +1,10 @@
 function newsDeskTagCreator(news_desk_value) {
-  if (news_desk_value === null) {
-    return new Type("Unlisted")
-  }
-  else {
-    return new Type(news_desk_value)
+  if (!store.types.includes(news_desk_value) && !store.types.includes("Unlisted")) {
+    if (news_desk_value === null) {
+      return new Type("Unlisted")
+    }
+    else  {
+      return new Type(news_desk_value)
+    }
   }
 }
