6a270301d8fd9e7b63cfc8e09eb4c4c8edd7d7e4
laushinka <laurie.malau@gmail.com>
2016-08-01 11:34:49 -0400

    Add filterByType() for dropdown

--- scripts/services/filter_by_type.js
+++ scripts/services/filter_by_type.js
@@ -1,3 +1,16 @@
 function filterByType(type){
-  debugger;
+  var dropType = type.children[0].innerHTML;
+  function findByTypeName(type) {
+    return type.name === dropType
+  }
+  var articleArr = store.types.find(findByTypeName).articles;
+
+  (function articleByTypeRender() {
+    let string = $('#article-template').html();
+    let template = Handlebars.compile(string);
+    var htmlString = template({articles: articleArr})
+    $('#results').empty();
+    $('#results').append(htmlString);
+  }());
+
 }
