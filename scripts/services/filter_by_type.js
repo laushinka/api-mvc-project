2b67f47ac9550061e86d723c21fe3b953f3904a5
ToddRizley <todd.rizley@gmail.com>
2016-08-02 10:25:45 -0400

    refactor filter by type

--- scripts/services/filter_by_type.js
+++ scripts/services/filter_by_type.js
@@ -1,19 +1,17 @@
 function filterByType(type){
-  // access the inner string
   var dropType = type.children[0].innerHTML;
-  // find the matching type
+  var articleArr = store.types.find(findByTypeName).articles;
+
   function findByTypeName(type) {
     return type.name === dropType
   }
-  // find the corresponding article
-  var articleArr = store.types.find(findByTypeName).articles;
-
-  (function articleByTypeRender() {
-    let string = $('#article-template').html();
-    let template = Handlebars.compile(string);
-    var htmlString = template({articles: articleArr})
-    $('#results').empty();
-    $('#results').append(htmlString);
-  }());
-
+  articleByTypeRender(articleArr)
 }
+
+function articleByTypeRender(array) {
+  let string = $('#article-template').html();
+  let template = Handlebars.compile(string);
+  var htmlString = template({articles: array})
+  $('#results').empty();
+  $('#results').append(htmlString);
+};
