ab68b01d12445d42581a5020d117fb5984bad332
ToddRizley <todd.rizley@gmail.com>
2016-08-02 09:37:59 -0400

    clean up code

--- scripts/models/type.js
+++ scripts/models/type.js
@@ -5,20 +5,3 @@ class Type {
     store.types.push(this)
   }
 }
-
-
-
-  // function displayPreferences() {
-  //   var types = this.types.forEach(t => t
-  //   ).join(', ')
-  //   return types
-  // }
-
-
-
-//retrieve information on all returned articles
-//parse JSON to find news_desk_values
-//store those values
-//add a selector to filter results
-//selector ONLY shows news_desk_values of results
-//add tag results with news_desk_values
